96ccde0cf536ff3efbabe62b1637b96155a0a1de
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-18 23:17:02 -0300

    rename token name achv

--- contracts/TheSeedCoin.sol
+++ contracts/TheSeedCoin.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-contract SpiritCoin {
+contract TheSeedCoin {
     
-    string public name = "Spirit Coin";
-    string public symbol = "SPT";
+    string public name = "The Seeds";
+    string public symbol = "SDS";
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000000 * 10 ** decimals;
 
---
+++ test/TheSeedCoin.test.ts
@@ -0,0 +1,88 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("TheSeedCoin", function () {
+  
+  async function deployFixture() {
+
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+
+    const TheSeedCoin = await hre.ethers.getContractFactory("TheSeedCoin");
+    const theSeedCoin = await TheSeedCoin.deploy();
+
+    return { theSeedCoin, owner, otherAccount };
+  }
+
+  describe("The Seeds Tests", () => {
+
+    it("Should be a correct name", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const name = await  theSeedCoin.name();
+      expect(name).to.equal("The Seeds");
+    });
+
+    it("Should be a correct symbol", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const symbol = await  theSeedCoin.symbol();
+      expect(symbol).to.equal("SDS");
+    });
+
+    it("Should be a correct decimals", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const decimals = await  theSeedCoin.decimals();
+      expect(decimals).to.equal(18);
+    });
+
+    it("Should be a correct totalSupply", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const totalSupply = await  theSeedCoin.totalSupply();
+      expect(totalSupply).to.equal(1000000n * 10n ** 18n);
+    });
+
+    it("Should get a balance", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const balance = await theSeedCoin.balanceOf(owner.address);
+      expect(balance).to.equal(1000000n * 10n ** 18n);
+    });
+
+    it("Should transfer", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const balanceOwnerBefore = await theSeedCoin.balanceOf(owner.address);
+      const balanceOtherAccountBefore = await theSeedCoin.balanceOf(otherAccount.address);
+
+      await theSeedCoin.transfer(otherAccount.address, 1n);
+
+      const balanceOwnerAfter = await theSeedCoin.balanceOf(owner.address);
+      const balanceOtherAccountrAfter = await theSeedCoin.balanceOf(otherAccount.address);
+
+      expect(balanceOwnerBefore).to.equal(1000000n * 10n ** 18n);
+      expect(balanceOwnerAfter).to.equal((1000000n * 10n ** 18n) - 1n);
+
+      expect(balanceOtherAccountBefore).to.equal(0);
+      expect(balanceOtherAccountrAfter).to.equal(1n);
+
+    });
+
+    it("Should NOT transfer", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      const instance = theSeedCoin.connect(otherAccount);
+      await expect(instance.transfer(owner, 1n)).to.revertedWith("Insufficient balance");
+
+    });
+
+    it("Should approve", async () => {
+      const { theSeedCoin, owner, otherAccount } = await loadFixture(deployFixture);
+      await theSeedCoin.approve(otherAccount, 1n);
+      const value = await theSeedCoin.allowance(owner, otherAccount);
+      expect(value).to.equal(1n);
+
+    });
+
+  });
+
+});
