d8317e64b585507ec7fb23e2b68f034055852580
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-18 23:25:28 -0300

    rename token name achv

--- contracts/TheSeedCoin.sol
+++ contracts/TheSeedCoin.sol
@@ -45,4 +45,17 @@ contract TheSeedCoin {
         return _allowances[_owner][_spender];
     }
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf(_from) >= _value, "Insufficient balance");
+        require(allowance(_from, msg.sender) >= _value, "Insufficient allowance");
+
+        _balances[_from] -= _value;
+        _allowances[_from][msg.sender] -= _value;
+        _balances[to] += _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
