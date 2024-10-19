9bb55291e8719404b7f3511d3ef5c0e6e2eb2cd7
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-18 22:35:17 -0300

    create: balanceOf and transfer methods

--- contracts/SpiritCoin.sol
+++ contracts/SpiritCoin.sol
@@ -11,4 +11,25 @@ contract SpiritCoin {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
+    mapping(address => uint256) public _balances;
+
+    constructor() {
+        _balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) public returns (uint256) {
+        return _balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf(msg.sender) >= _value, "Insufficient balance");
+
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
 }
